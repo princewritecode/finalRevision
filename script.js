4e9ac862365b3ba2a3ef8f4ddc529bbe9d4c0f72
princewritecode <princewritecode@gmail.com>
2023-11-07 15:23:12 +0530

    basics of js added

---
+++ script.js
@@ -0,0 +1,32 @@
+// all about javscript
+
+console.log(0 / 0);
+console.log(1 / 0);
+console.log(Infinity / 0);
+console.log(typeof NaN);
+
+let score = 0;
+score += 10;
+score;
+score *= 2;
+score;
+
+
+// string A string is simply a sequence of textual characters.
+
+const text = "The ' character is called an apostrophe.";
+text;
+
+console.log("hello this is log method in console object");
+
+// swapping a numbers
+
+let a = 20;
+let b = 22;
+let c;
+c = a;
+a = b;
+b = c;
+a;
+b
+
